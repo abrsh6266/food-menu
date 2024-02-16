afbd56a15ae63346edabd3d2cb99097b43b8b3ff
Abrham <abrsh6266@gmail.com>
2024-02-15 21:53:27 -0900

    creating menu component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
+import { useState } from 'react';
+import data from './data'
 const App = () => {
+  const [foods,setFoods] = useState(data)
+  console.log(foods)
   return <h2>Menu Starter</h2>;
 };
 export default App;
---
+++ src/Menu.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Menu = ({foods}) => {
+  return (
+    <div>Menu</div>
+  )
+}
+
+export default Menu
